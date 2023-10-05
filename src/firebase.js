edd747d3136a73571ac4d7e6f196ba038f46181a
Gaby <gabriela.noriega@alumnos.udg.mx>
2023-10-05 10:08:34 -0600

    ultimos arreglos CSS color botones

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth'; 
+import { getAuth,  signOut as firebaseSignOut } from 'firebase/auth'; 
 import { getFirestore } from 'firebase/firestore';
-
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -24,3 +23,4 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { app, db, getAuth };
+export { firebaseSignOut as signOut };
